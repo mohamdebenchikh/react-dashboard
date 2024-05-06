69d93318c703331aefbec89e0fba773b9ea10678
mohamdebenchikh <mohamdebenchikh.dev@gmail.com>
2024-05-06 23:49:41 +0200

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,9 @@ function App() {
       <Sidebar show={openSidebar} onClose={closeSidebar}></Sidebar>    
       <main className={`transition-all ease-linear duration-300 ${openSidebar ? "ml-0 md:ml-72"  : "ml-0" }`}>
         <Header toggleSidebar={() => setOpenSidebar(curr => !curr)}></Header>
-        
+          <div className="py-8 px-6">
+            <h1 className="text-gray-900 underline text-3xl dark:text-gray-100">Hello world</h1>
+          </div>
         </main>  
     </div>
   );
